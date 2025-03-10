83c3ce8ddb161e5318fcdfc505e4780183778567
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2025-03-09 20:24:19 -0400

    update vite.config for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  root: ".", // Ensures Vite starts from the correct folder
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index copy.html", // Tell Vite to use "index copy.html"
+      },
+    },
+    outDir: "dist", // Output directory
+  },
+});
+
+
